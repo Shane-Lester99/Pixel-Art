1141f31e799c9107c06f5793f033c69b275bd0b8
Shane Lester <Shanejlester@gmail.com>
2018-06-29 17:10:25 -0400

    Init, form can create grid dynamicly according to user input. Colors currently not working.

---
+++ designs.js
@@ -0,0 +1,83 @@
+// Select color input
+
+// Select size input
+//const form = document.querySelector('form');
+/*
+form.addEventListener('change', function test() {
+  const curr = event.target;
+  alert(curr.value);
+  //curr.value = event.target.value;
+  /*alert(document.body.querySelector('input[name="height"]'.valye); */
+//});*/
+
+
+/*
+const inputBox = document.querySelectorAll('input[type="number"]');
+
+inputBox.addEventListener('onchange', function updateBox() {
+	alert('box was changed'); 
+});
+*/
+//heightBox.value = 3;
+//inputBox[0].value = 5;
+
+//alert(inputBox[0].value);
+
+// When size is submitted by the user, call makeGrid()
+/*document.addEventListener('click', function test() { alert("Hello world"); });
+*/
+
+function paintColor() {
+	//event.target.style.color = "red";
+  alert('activate paintColor');
+}
+
+//Submit Button
+document.querySelector('input[type="submit"]').addEventListener('click', function test(event) {
+  event.preventDefault();
+  const boxes = document.querySelectorAll('input[type="number"');
+  heightOfGrid = boxes[0].value;
+  widthOfGrid = boxes[1].value;
+  makeGrid(heightOfGrid, widthOfGrid);
+  /*const height = document.querySelector('#inputHeight') {
+  //height.setAttribute('value') = 7;
+  alert(height);
+  }*/
+});
+
+
+
+function makeGrid(height, width) {
+  // Your code goes here!
+  /*
+  const grid = document.querySelector("#pixelCanvas");
+  const row = document.createElement("TR");
+  row.insertCell(0);
+  row.insertCell(1);
+  grid.appendChild(row);
+  
+  alert(grid.outerHTML);
+  */
+  //alert(grid.outerHTML);
+  
+  
+  const grid = document.querySelector('#pixelCanvas');
+  /*
+  if (EMPTYGRID !== grid.outerHTML) {
+    alert("HIT1");
+    document.querySelector('#pixelCanvas').outerHTML = EMPTYGRID; 
+    alert("HIT2");   
+  }*/
+  grid.innerHTML = "";
+  for (let i = 0; i < height; i++) {
+    const newRow = 	document.createElement("TR");
+    
+    for (let j = 0; j < width; j++) {
+      newRow.insertCell(j);
+     
+    }   
+    grid.appendChild(newRow);
+  }
+}
+
+
